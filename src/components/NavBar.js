40ed7b7a08187c1147b8fe8fe73898925b623e6d
baharehforoughi <bhr.foroughi@gmail.com>
2024-01-22 16:58:21 +0100

    NavBar with NavLink and style

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,18 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 const NavBar = () => {
   return (
     <>
-      <Link to="/">Home</Link>
+      <NavLink className="navItem" to="/">
+        Home
+      </NavLink>
       {"-"}
-      <Link to="/discover">Discover</Link>
+      <NavLink className="navItem" to="/discover">
+        Discover
+      </NavLink>
       {"-"}
-      <Link to="/about">About</Link>
+      <NavLink className="navItem" to="/about">
+        About
+      </NavLink>
     </>
   );
 };
